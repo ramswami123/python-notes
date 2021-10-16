e7b1b27da943b7147d383be5ad275cdb09692573
ramswami123 <75007299+ramswami123@users.noreply.github.com>
2021-10-16 11:43:08 +0530

    Add files via upload

---
+++ is_in_python.py
@@ -0,0 +1,16 @@
+#in c++ we use == to compare to valued equal or not
+#but in python we use "is" keyword 
+a=70
+b=40
+c=70
+if a is b:
+    print("a==b in c++")
+else:
+    print("a and b are not equal")
+
+if a is c:
+    print("a==c in c++")
+#output
+#a and b are not equal
+#a==c in c++
+    
